961350265750eb969ec878e7f894b963b8068a27
“bhtrung” <tonik8765@gmail.com>
2026-04-08 13:13:06 +0200

    prod fix 2

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { PAYMENT_INFO } from "./campData.ts";
+import { PAYMENT_INFO } from "./campData.js";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
